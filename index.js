780e7f476a27f46fb6e238058504fff282005d5a
Anartz Mugika <mugan86@gmail.com>
2018-12-01 19:09:21 +0100

    Módulo sencillo con saludos creado

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    hola: function() {
+        console.log('Hola Mundo!!!');
+    },
+    holaPersonalizado: function(nombre) {
+        console.log(`Hola ${ nombre }`);
+    },
+    holaAlCursoNPM: function(){
+        console.log('Hola al curso NPM!!!');
+    }
+}
